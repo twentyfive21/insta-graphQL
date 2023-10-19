94c4a3ff34a5cfab24fceb934189837d821bdce3
Noel Guillen <noelblas274@gmail.com>
2023-10-19 17:15:10 -0400

    added UI for login

--- src/pages/UserLogin/UserLogin.jsx
+++ src/pages/UserLogin/UserLogin.jsx
@@ -1,5 +1,7 @@
 import React, {useState} from 'react'
 import './UserLogin.css'
+import phone from "../../assets/login/instaPhone.png";
+import logo from "../../assets/login/Instagram_logo.svg.png";
 
 function UserLogin() {
 
@@ -7,8 +9,29 @@ function UserLogin() {
    const [signUp, setSignUp] = useState(false)
 
   return (
-    <div>
-    WELCOME 
+    <div className='login-div-container'>
+      <div className='phone-container'>
+        <img src={phone} alt='phone'/>
+      </div>
+      <div className='login-container'>
+        <img src={logo} alt='logo'/>
+          <div className='login-form-container'>
+            <input type="text" className='form-input' placeholder='Email'/>
+            <input type="text" className='form-input' placeholder='Password'/>
+            <button className='login-btn'>Log In</button>
+          </div>
+          <div className='login-or-option'>
+            <p className='line'></p>
+            <p>OR</p>
+            <p className='line'></p>
+          </div>
+          <div className='optional-box'>
+            <p>Don't Have an account? <span>Sign Up</span></p>
+          </div>
+          <div className='get-the-app'>
+
+          </div>
+      </div>
     </div>
   )
 }
