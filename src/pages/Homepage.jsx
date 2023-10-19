44d9dd8e1b202ab246a7c6ef72ca47e1756dd26b
Noel Guillen <noelblas274@gmail.com>
2023-10-18 21:36:16 -0400

    fixed UI main styles

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -26,18 +26,9 @@ function Homepage({userData}) {
           <Posts item={userData[3].users[2]} index={2}/>
           </div>
       </div>
-      <div className='follow-right'>
+      {/* <div className='follow-right'>
           <Follow item={userData[0].users[0]} style={{backgroundColor: '#F5F5F5'}}   />
-          {/* <div className='right-sug'>
-          <p>Suggestions for you</p>
-          <p>See All</p>
-          </div> */}
-          {/* <Follow item={userData[0].users[1]}/>
-          <Follow item={userData[0].users[2]}/>
-          <Follow item={userData[0].users[3]}/>
-          <Follow item={userData[0].users[4]}/>
-          <Follow item={userData[0].users[5]}/> */}
-      </div>
+      </div> */}
     </section>
     </div>
   )
