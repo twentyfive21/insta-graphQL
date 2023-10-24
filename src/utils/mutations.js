e9ed442442103c1747ec41cc51afe0e07e7975db
twentyfive21 <tina.winnn@gmail.com>
2023-10-23 21:35:22 -0400

    added post mutations and subscription created from hasura

--- src/utils/mutations.js
+++ src/utils/mutations.js
@@ -16,3 +16,33 @@ export const ADD_USER = gql`
     }
   }
 `;
+
+export const ADD_POST = gql`
+mutation addPost($caption: String!, $image: String!, $userID: uuid!) {
+  insert_userPosts(
+    objects: { caption: $caption, image: $image, userID: $userID }
+    ) {
+    returning {
+      caption
+      id
+      image
+      likes
+      userID
+      timestamp
+    }
+  }
+}
+`;
+
+export const DELETE_POST = gql`
+mutation deletePost($id: uuid!, $userID: uuid!) {
+  delete_userPosts(
+    where: {id: {_eq: $id}, userID: {_eq: $userID}}
+    ) {
+    returning {
+      id
+      userID
+    }
+  }
+}
+`;
