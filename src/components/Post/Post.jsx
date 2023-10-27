992c2018f87f3fe055776b54435a1d0faa74dd40
Noel Guillen <noelblas274@gmail.com>
2023-10-26 22:33:03 -0400

    post jsx updated for user only

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -4,20 +4,23 @@ import Comment from '../Comment/Comment'
 import Actions from '../Actions/Actions'
 import AddComment from '../AddComment/AddComment'
 import leftNav from '../../assets/leftnav.png'
+import { userData as oldData } from '../../utils/data'
+import Collab from '../Collab/Collab'
 
 function Post({userData}) {
   return (
     <div className='main-box'>
         <section className='next-img'>
-        <img src={userData[0].post} alt={userData[0].username}/>
-        <img src={leftNav} className='next-btn' alt='next-btn'/>
+        <img src={userData?.image} alt={userData?.userName} className='post-main-image'/>
+        {/* <img src={leftNav} className='next-btn' alt='next-btn'/> */}
         </section>
         <section>
-        <Comment userData={userData[0]} />
-        <Comment userData={userData[1]} />
-        <Comment userData={userData[2]} size={{width:'40px', height:'40px'}} />
-        <Comment userData={userData[3]} />
-        <Actions userData={userData[4]} />
+        <Collab userData={oldData}/>
+        <Comment userData={oldData[0]} />
+        <Comment userData={oldData[1]} />
+        <Comment userData={oldData[2]} size={{width:'40px', height:'40px'}} />
+        <Comment userData={oldData[3]} />
+        <Actions userData={oldData[4]} />
         <AddComment />
         </section>
     </div>
