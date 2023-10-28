0d77c3f088da7ca1c2530c3469b62e1ebfb3fd40
Noel Guillen <noelblas274@gmail.com>
2023-10-28 18:55:14 -0400

    added new css

---
+++ src/CustomWebcam.jsx
@@ -0,0 +1,12 @@
+// import Webcam from "react-webcam";
+// import { useRef } from "react";
+
+// const CustomWebcam = () => {
+//    const webcamRef = useRef(null); 
+//   return (
+//     <div className="cam-container">
+//       <Webcam height={600} width={600} ref={webcamRef} />
+//     </div>
+//   );
+// };
+// export default CustomWebcam;
