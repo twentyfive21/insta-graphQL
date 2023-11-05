3555300c9bb4e2f55cc4203889f455474ca1dc17
Noel Guillen <noelblas274@gmail.com>
2023-11-04 21:09:37 -0500

    APP jsx prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import './App.css'
-import Homepage from '../src/pages/Homepage/Homepage'
-import { userPostData } from './utils/data'
-import { Route, Routes, BrowserRouter } from 'react-router-dom'
-import UserLogin from './pages/UserLogin/UserLogin'
-import ProfilePage from './pages/ProfilePage/ProfilePage'
-import UserSignUp from './pages/UserSignUp/UserSignUp'
-import UserContextProvider from './contexts/CurrentUser'
-import CommentsContextProvider from './contexts/CommentData'
-import PostContextProvider from './contexts/PostContext'
-import LikesContextProvider from './contexts/LikesContext'
+import { useState } from "react";
+import "./App.css";
+import Homepage from "../src/pages/Homepage/Homepage";
+import { userPostData } from "./utils/data";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import UserLogin from "./pages/UserLogin/UserLogin";
+import ProfilePage from "./pages/ProfilePage/ProfilePage";
+import UserSignUp from "./pages/UserSignUp/UserSignUp";
+import UserContextProvider from "./contexts/CurrentUser";
+import CommentsContextProvider from "./contexts/CommentData";
+import PostContextProvider from "./contexts/PostContext";
+import LikesContextProvider from "./contexts/LikesContext";
 
 function App() {
   return (
-      <UserContextProvider>
+    <UserContextProvider>
       <PostContextProvider>
-      <CommentsContextProvider>
-      <LikesContextProvider>
-      <BrowserRouter>
-        <Routes>
-        <Route path='/' element={<UserLogin />} />
-        <Route path='/sign-up' element={<UserSignUp />} />
-        <Route path="/feed" element={<Homepage userData={userPostData} />} />
-        <Route path="/profile-page/:userid" element={<ProfilePage />} />
-        </Routes>
-      </BrowserRouter>
-      </LikesContextProvider>
-      </CommentsContextProvider>
+        <CommentsContextProvider>
+          <LikesContextProvider>
+            <BrowserRouter>
+              <Routes>
+                <Route path="/" element={<UserLogin />} />
+                <Route path="/sign-up" element={<UserSignUp />} />
+                <Route
+                  path="/feed"
+                  element={<Homepage userData={userPostData} />}
+                />
+                <Route path="/profile-page/:userid" element={<ProfilePage />} />
+              </Routes>
+            </BrowserRouter>
+          </LikesContextProvider>
+        </CommentsContextProvider>
       </PostContextProvider>
-      </UserContextProvider>
-  )
+    </UserContextProvider>
+  );
 }
 
-export default App
+export default App;
