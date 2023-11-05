2bd14b2e56acb59a2c7ac9cdcf3c7aa8a51bc49d
Noel Guillen <noelblas274@gmail.com>
2023-11-04 21:10:43 -0500

    Post jsx prettier

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,34 +1,41 @@
-import {useContext} from 'react'
-import './Post.css'
-import Comment from '../Comment/Comment'
-import Actions from '../Actions/Actions'
-import leftNav from '../../assets/leftnav.png'
-import Collab from '../Collab/Collab'
-import { CommentsContext } from '../../contexts/CommentData'
+import { useContext } from "react";
+import "./Post.css";
+import Comment from "../Comment/Comment";
+import Actions from "../Actions/Actions";
+import Collab from "../Collab/Collab";
+import { CommentsContext } from "../../contexts/CommentData";
 
-function Post({userData, userLike}) {
-  const {commentTable} = useContext(CommentsContext);
- 
+function Post({ userData, userLike }) {
+  const { commentTable } = useContext(CommentsContext);
+
   const filteredComments = commentTable?.filter(
-    (comment) => comment?.postRef === userData.id
+    (comment) => comment?.postRef === userData?.id
   );
-  
+
   return (
-    <div className='main-box'>
-        <section className='next-img'>
-        <img src={userData?.image} alt={userData?.userName} className='post-main-image'/>
+    <div className="main-box">
+      <section className="next-img">
+        <img
+          src={userData?.image}
+          alt={userData?.userName}
+          className="post-main-image"
+        />
+      </section>
+      <section>
+        <Collab userData={userData} />
+        <section className="all-comments-container">
+          {filteredComments?.map((item) => {
+            return <Comment userData={item} key={item?.id} />;
+          })}
         </section>
-        <section>
-        <Collab userData={userData}/>
-          <section className='all-comments-container'>
-            {filteredComments.map((item)=>{
-              return <Comment userData={item} key={item.id}/>
-            })}
-          </section>
-        <Actions postID={userData.id} userLike={userLike} userData={userData}/>
-        </section>
+        <Actions
+          postID={userData?.id}
+          userLike={userLike}
+          userData={userData}
+        />
+      </section>
     </div>
-  )
+  );
 }
 
-export default Post
+export default Post;
