7b3b13d391bd7ecf71f14be73e41f177b238c2c6
Noel Guillen <noelblas274@gmail.com>
2023-11-05 21:18:11 -0500

    jsx prettier

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { ApolloProvider } from '@apollo/client'
-import client from './utils/client.js'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { ApolloProvider } from "@apollo/client";
+import client from "./utils/client.js";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ApolloProvider client={client}>
       <App />
     </ApolloProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
