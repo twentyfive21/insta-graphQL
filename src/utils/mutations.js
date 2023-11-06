1e56537d23aec8bbbc052988933d5b857e6178ec
Noel Guillen <noelblas274@gmail.com>
2023-11-05 21:18:39 -0500

    jsx prettier

--- src/utils/mutations.js
+++ src/utils/mutations.js
@@ -13,103 +13,128 @@ export const ADD_USER = gql`
 `;
 
 export const ADD_AVATAR = gql`
-mutation changeAvatar($id: uuid!, $avatar: String!) {
-  update_userData(
-    where: { id: { _eq: $id } }
-    _set: { avatar: $avatar }
-  ) {
-    returning {
-      id
+  mutation changeAvatar($id: uuid!, $avatar: String!) {
+    update_userData(where: { id: { _eq: $id } }, _set: { avatar: $avatar }) {
+      returning {
+        id
+      }
     }
   }
-}
 `;
 
 export const ADD_POST = gql`
-mutation addPost($caption: String!, $image: String!, $userID: uuid!, $avatar: String!, $username: String!) {
-  insert_userPosts(
-    objects: { caption: $caption, image: $image, userID: $userID, avatar: $avatar, username: $username}
+  mutation addPost(
+    $caption: String!
+    $image: String!
+    $userID: uuid!
+    $avatar: String!
+    $username: String!
+  ) {
+    insert_userPosts(
+      objects: {
+        caption: $caption
+        image: $image
+        userID: $userID
+        avatar: $avatar
+        username: $username
+      }
     ) {
-    returning {
-      userID
+      returning {
+        userID
+      }
     }
   }
-}
 `;
 
 export const ADD_COMMENT = gql`
-mutation addComment($avatar: String!, $comment: String!, $postRef: uuid!, $userID: uuid!, $username: String!) {
-  insert_userComments(
-    objects: {avatar: $avatar, comment: $comment, postRef: $postRef, userID: $userID, username: $username}) {
-    returning {
-      comment
+  mutation addComment(
+    $avatar: String!
+    $comment: String!
+    $postRef: uuid!
+    $userID: uuid!
+    $username: String!
+  ) {
+    insert_userComments(
+      objects: {
+        avatar: $avatar
+        comment: $comment
+        postRef: $postRef
+        userID: $userID
+        username: $username
+      }
+    ) {
+      returning {
+        comment
+      }
     }
   }
-}
 `;
 
 export const DELETE_COMMENT = gql`
-mutation deleteComment($postRef: uuid!, $userID: uuid! $id: uuid){
-  delete_userComments(
-    where: {postRef: {_eq: $postRef}, userID: {_eq: $userID}, id: {_eq: $id}}) {
-    returning {
-      comment
+  mutation deleteComment($postRef: uuid!, $userID: uuid!, $id: uuid) {
+    delete_userComments(
+      where: {
+        postRef: { _eq: $postRef }
+        userID: { _eq: $userID }
+        id: { _eq: $id }
+      }
+    ) {
+      returning {
+        comment
+      }
     }
   }
-}
 `;
 
 export const SET_LIKE = gql`
-mutation setLikedPhoto($userID: uuid!, $postRef: uuid!) {
-  insert_userLikes(
-    objects: {userID: $userID, postRef: $postRef}) {
-    returning {
-      isLiked
+  mutation setLikedPhoto($userID: uuid!, $postRef: uuid!) {
+    insert_userLikes(objects: { userID: $userID, postRef: $postRef }) {
+      returning {
+        isLiked
+      }
     }
   }
-}
 `;
 
 export const REMOVE_LIKE = gql`
-mutation removeLike($postRef: uuid!, $userID: uuid!) {
-  delete_userLikes(
-    where: {postRef: {_eq: $postRef}, userID: {_eq: $userID}}) {
-    returning {
-      postRef
+  mutation removeLike($postRef: uuid!, $userID: uuid!) {
+    delete_userLikes(
+      where: { postRef: { _eq: $postRef }, userID: { _eq: $userID } }
+    ) {
+      returning {
+        postRef
+      }
     }
   }
-}
 `;
 
 // THE FOLLOWING MUTATIONS ARE DELETE ALL MUTATIONS WHEN A POST IS DELETED !!
 export const DELETE_POST_LIKES = gql`
-mutation deleteAllLikesForPhoto($postRef: uuid!) {
-  delete_userLikes(where: {postRef: {_eq: $postRef }}) {
-    returning {
-      postRef
+  mutation deleteAllLikesForPhoto($postRef: uuid!) {
+    delete_userLikes(where: { postRef: { _eq: $postRef } }) {
+      returning {
+        postRef
+      }
     }
   }
-}
 `;
 
 export const DELETE_POST_COMMENTS = gql`
-mutation deleteSelectedPostComments($postRef: uuid!) {
-  delete_userComments(where: {postRef: {_eq: $postRef}}) {
-    returning {
-      postRef
+  mutation deleteSelectedPostComments($postRef: uuid!) {
+    delete_userComments(where: { postRef: { _eq: $postRef } }) {
+      returning {
+        postRef
+      }
     }
   }
-}
 `;
 
 export const DELETE_POST = gql`
-mutation deletePost($id: uuid!, $userID: uuid!) {
-  delete_userPosts(
-    where: {id: {_eq: $id}, userID: {_eq: $userID}}
-    ) {
-    returning {
-      userID
+  mutation deletePost($id: uuid!, $userID: uuid!) {
+    delete_userPosts(where: { id: { _eq: $id }, userID: { _eq: $userID } }) {
+      returning {
+        userID
+      }
     }
   }
-}
 `;
